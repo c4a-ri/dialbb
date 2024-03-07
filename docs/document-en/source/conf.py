6d654b0b5e693f23f8bcc7c3545719fa5a0d48a8
Mikio Nakano <mikio.nakano@c4a.jp>
2024-03-07 17:45:08 +0900

    doc for ver0.7

--- docs/document-en/source/conf.py
+++ docs/document-en/source/conf.py
@@ -23,7 +23,7 @@ copyright = 'C4A Research Institute, Inc'
 author = 'Mikio Nakano'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.6.0'
+release = 'v0.7.0'
 
 
 # -- General configuration ---------------------------------------------------
