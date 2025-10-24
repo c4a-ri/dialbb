eb8e159859a5430f565d1d8772ecc34d1a3fdf21
Mikio Nakano <mikio.nakano@c4a.jp>
2025-10-24 16:53:35 +0900

    wrong changes reverted

--- scripts/zip-dialbb-setup.bat
+++ scripts/zip-dialbb-setup.bat
@@ -1,5 +1,5 @@
 set current=%cd%
 cd dist
 del ..\docs\files\dialbb-setup.zip
-zip -r ..\docs\files\dialbb-setup.zip dialbb*.whl README.txt install.bat uninstall.bat start-dialbb-nc.bat 
+zip -r ..\docs\files\dialbb-setup.zip dialbb*.whl README.txt install.bat uninstall.bat start-dialbb-nc.bat install-scenario-editor.bat
 cd %current%
