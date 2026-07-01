1d8b536cc6dbd4fe058a8f126d7859b2f07b898b
mk-nakano <mk-nakano@users.noreply.github.com>
2026-07-01 07:23:36 +0000

    deploy: 212b47881c42c5838c43d4520b6670d77df53949

--- document-ja/build/html/_static/documentation_options.js
+++ document-ja/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.2.0',
+    VERSION: 'v1.2.4',
     LANGUAGE: 'ja',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- document-ja/source/conf.py
+++ document-ja/source/conf.py
@@ -23,7 +23,7 @@ copyright = 'C4A Research Institute, Inc'
 author = 'Mikio Nakano'
 
 # The full version, including alpha/beta/rc tags
-release = 'v1.2.0'
+release = 'v1.2.4'
 
 
 # -- General configuration ---------------------------------------------------
